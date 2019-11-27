2c2670610a40753069b2272cde97eaa936e70cc1
roxreis <rodrigoassuncao12@gmail.com>
2019-11-27 10:41:07 -0200

    criado migrations products e products images

--- database/migrations/2019_11_27_115459_create_products_image.php
+++ database/migrations/2019_11_27_115459_create_products_image.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProducts extends Migration
+class CreateProductsImage extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateProducts extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('products_image', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 300);
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('url_image',300);
             $table->string('description',300);
-            $table->integer('quantity');
-            $table->float('price', 10, 2);
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateProducts extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('products_image');
     }
 }
